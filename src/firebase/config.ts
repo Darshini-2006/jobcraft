3dafccb32e399f07d70ba82fd1dab9dea6f19a84
Darshini Rathinavel <darshinirathinavel75@gmail.com>
2025-11-14 15:25:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2085221274-4fef2",
+  "appId": "1:83729275130:web:16d007218246fa15acd143",
+  "apiKey": "AIzaSyCf9J3rY05Ft3FmJPDINGloKAz1DC9Lgrw",
+  "authDomain": "studio-2085221274-4fef2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "83729275130"
+};
